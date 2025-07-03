a05cfd83abedd546461a16b6ff59b0192fdd5dc9
Rina <kriskeih.ands@gmail.com>
2025-07-03 23:04:17 +0300

    добавлена логика brain-even и аскинема brain-even

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ri1r/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ri1r/frontend-project-44/actions)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ri1r_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ri1r_frontend-project-44)
+[![asciinema](https://asciinema.org/a/OKV7nem0WkIJavjHNHhYJtBJ8.svg)](https://asciinema.org/a/OKV7nem0WkIJavjHNHhYJtBJ8)
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
 
-import greetUser from "../src/cli.js";
+import readlineSync from 'readline-sync';
 
-greetUser();
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+let correctAnswers = 0;
+
+while (correctAnswers < 3) {
+  const number = Math.floor(Math.random() * 100) + 1;
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    correctAnswers += 1;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+}
+
+if (correctAnswers === 3) {
+  console.log(`Congratulations, ${name}!`);
+}
